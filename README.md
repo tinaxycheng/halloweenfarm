6bd0ca30550c86cff6984d2f99dcdc633b5e13b4
Xiaoyong Cheng <tinaxycheng@gmail.com>
2018-11-01 18:37:32 -0400

    new background and menu

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-Created by Codrops
-
-Please read about our license: http://tympanus.net/codrops/licensing/
+Created by Tina Cheng
+Halloween farm with pumpkins and sound effects
--- js/farm.js
+++ js/farm.js
@@ -1,9 +1,81 @@
 
 
 
-$('.person').click(function() {
-    $('.pumpkin').addClass('drop');
+$('.pumpkin-0').click(function() {
+    $('.pumpkin-0').animate({'top':'600px'}, 100);
 });
+$('.pumpkin-1').click(function() {
+    $('.pumpkin-1').animate({'top':'600px'}, 100);
+});
+$('.pumpkin-2').click(function() {
+    $('.pumpkin-2').animate({'top':'600px'}, 100);
+});
+$('.pumpkin-3').click(function() {
+    $('.pumpkin-3').animate({'top':'500px'}, 100);
+});
+$("#eyeglass").mouseenter(function() {
+	$(this).animate({'opacity':'0.3'}, 100);
+});
+$("#tie").mouseenter(function() {
+	$(this).animate({'opacity':'0.5'}, 100);
+});
+
+var open = false;
+var home =  document.getElementById("home");
+
+home.addEventListener( 'click', function( event ) {
+				if( !open ) {
+				$(".homefarmhours").show();
+					open = true;
+				} 
+				else{
+				$(".homefarmhours").hide();
+					open = false;
+				};
+			}, false );
+
+
+var pickyourown =  document.getElementById("pickyourown");
+
+pickyourown.addEventListener( 'click', function( event ) {
+				if( !open ) {
+				$(".pickyourown").show();
+					open = true;
+				} 
+				else{
+				$(".pickyourown").hide();
+					open = false;
+				};
+			}, false );
+
+
+var festival =  document.getElementById("festival");
+
+festival.addEventListener( 'click', function( event ) {
+				if( !open ) {
+				$(".festival").show();
+					open = true;
+				} 
+				else{
+				$(".festival").hide();
+					open = false;
+				};
+			}, false );
+
+var contact =  document.getElementById("contact");
+
+contact.addEventListener( 'click', function( event ) {
+				if( !open ) {
+				$(".contact-info").show();
+					open = true;
+				} 
+				else{
+				$(".contact-info").hide();
+					open = false;
+				};
+			}, false );
+
+
 
 
 var audio = document.getElementById("brook");
